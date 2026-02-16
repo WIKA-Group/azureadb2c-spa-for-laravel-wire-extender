45e419719fb71918d2baf1f62807efa4ef9e0292
David Hein <david.hein@wika.com>
2026-02-16 13:12:14 +0100

    PackageServiceProvider: Added required package name

--- src/AzureAdB2cWireExtenderServiceProvider.php
+++ src/AzureAdB2cWireExtenderServiceProvider.php
@@ -4,7 +4,10 @@ namespace WikaGroup\AzureAdB2cWireExtender;
 
 class AzureAdB2cWireExtenderServiceProvider extends \Spatie\LaravelPackageTools\PackageServiceProvider
 {
-    public function configurePackage(\Spatie\LaravelPackageTools\Package $package): void {}
+    public function configurePackage(\Spatie\LaravelPackageTools\Package $package): void {
+        $package
+            ->name('azureB2cWireExtender');
+    }
 
     public function packageBooted()
     {
